d8d076ae0402e393ef5b4d6e03731bf7958905b3
a22zacno <a22zano@studnet.his.se>
2023-05-23 18:52:12 +0200

    added a the fatcat drawable to the imageview so that it may be seen

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
